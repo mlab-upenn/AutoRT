b40060d4f26d5d2de74c909174d549723397a658
jainachin <achinjain1889@gmail.com>
2015-09-02 22:08:58 -0400

    Changed Sampling Time

--- Code/ARMAXTuning.m
+++ Code/ARMAXTuning.m
@@ -5,9 +5,13 @@ clear
 filename = '5ZoneSteamBaseboard.csv';
 [~,~,RawData] = xlsread(filename);
 
-% First 48 numeric entries in the excel file correspond to design days.
-% These must be excluded.
-StartIndex = 242;
+% First 48*60/TimeStep numeric entries in the excel file correspond to design days
+% These must be excluded. Higher start index can be chosen to account for
+% autoregrssive part
+
+TimeStep = 15; % In mins
+OrderOfAR = 10*60/TimeStep; % less than 24 hrs here
+StartIndex = 1+2*24*60/TimeStep+24*60/TimeStep+1; % 1 for string names, 2 days for design days data, then autoregressive terms, then 1 for next index
 
 %% Organize the data
 
@@ -17,18 +21,18 @@ InputData = {};
 % Training Features without autoregressive contribution
 % Specify the features from EnergyPlus .idf output variables
 % TrainingData{end+1}.Name = '';
-InputData{end+1}.Name = 'Environment:Site Outdoor Air Drybulb Temperature [C](Hourly)';
-% InputData{end+1}.Name = 'SPACE1-1:Zone Air System Sensible Heating Rate [W](Hourly)';
-% InputData{end+1}.Name = 'SPACE1-1:Zone Air Heat Balance Air Energy Storage Rate [W](Hourly)';
-% InputData{end+1}.Name = 'SPACE1-1:Zone Total Internal Total Heating Rate [W](Hourly)';
-% InputData{end+1}.Name = 'Environment:Site Direct Solar Radiation Rate per Area [W/m2](Hourly)';
-InputData{end+1}.Name = 'FRONT-1:Surface Outside Face Incident Solar Radiation Rate per Area [W/m2](Hourly)';
-InputData{end+1}.Name = 'Environment:Site Outdoor Air Relative Humidity [%](Hourly)';
-InputData{end+1}.Name = 'Environment:Site Wind Speed [m/s](Hourly)';
-InputData{end+1}.Name = 'Environment:Site Wind Direction [deg](Hourly)';
-InputData{end+1}.Name = 'SPACE1-1:Zone People Occupant Count [](Hourly)';
-InputData{end+1}.Name = 'SPACE1-1:Zone Lights Total Heating Energy [J](Hourly)';
-InputData{end+1}.Name = 'SPACE1-1 BASEBOARD:Baseboard Total Heating Rate [W](Hourly)';
+InputData{end+1}.Name = 'Environment:Site Outdoor Air Drybulb Temperature [C](TimeStep)';
+% InputData{end+1}.Name = 'SPACE1-1:Zone Air System Sensible Heating Rate [W](TimeStep)';
+% InputData{end+1}.Name = 'SPACE1-1:Zone Air Heat Balance Air Energy Storage Rate [W](TimeStep)';
+% InputData{end+1}.Name = 'SPACE1-1:Zone Total Internal Total Heating Rate [W](TimeStep)';
+% InputData{end+1}.Name = 'Environment:Site Direct Solar Radiation Rate per Area [W/m2](TimeStep)';
+InputData{end+1}.Name = 'FRONT-1:Surface Outside Face Incident Solar Radiation Rate per Area [W/m2](TimeStep)';
+InputData{end+1}.Name = 'Environment:Site Outdoor Air Relative Humidity [%](TimeStep)';
+InputData{end+1}.Name = 'Environment:Site Wind Speed [m/s](TimeStep)';
+InputData{end+1}.Name = 'Environment:Site Wind Direction [deg](TimeStep)';
+InputData{end+1}.Name = 'SPACE1-1:Zone People Occupant Count [](TimeStep)';
+InputData{end+1}.Name = 'SPACE1-1:Zone Lights Total Heating Energy [J](TimeStep)';
+InputData{end+1}.Name = 'SPACE1-1 BASEBOARD:Baseboard Total Heating Rate [W](TimeStep)';
 
 for idx = 1:size(InputData,2)
     for idy = 1:size(RawData,2)
@@ -44,12 +48,12 @@ NoOfDataPoints = size(InputData{1}.Data,1);
 % Example: Start Day  = Monday
 % Mon = 0, Tue = 1, Wed = 2, Thur = 3, Fri = 4, Sat = 5, Sun = 6
 
-NoOfDays = NoOfDataPoints/24;
+NoOfDays = NoOfDataPoints/(24*60/TimeStep);
 InputData{end+1}.Name = 'Day';
 
 x1 = [0:NoOfDataPoints-1]';
-x2 = mod(x1,24);
-InputData{end}.Data = mod((x1-x2)/24,7);
+x2 = mod(x1,(24*60/TimeStep));
+InputData{end}.Data = mod((x1-x2)/(24*60/TimeStep),7);
 
 % Time of day
 InputData{end+1}.Name = 'Time';
@@ -57,14 +61,14 @@ for idy = 1:size(RawData,2)
     if strcmp(RawData{1,idy}, 'Date/Time')
         InputData{end}.Data = RawData(StartIndex:end,idy);
         chartime = char(InputData{end}.Data);
-        InputData{end}.Data = str2num(chartime(:,9:10)); %#ok<ST2NM>
+        InputData{end}.Data = str2num(chartime(:,9:10))*60+str2num(chartime(:,12:13)); %#ok<ST2NM>
     end
 end
 
 % Output Variables
 NoOfOutput = 1;
 OutputData = {};
-OutputData{end+1}.Name = 'SPACE1-1:Zone Air Temperature [C](Hourly)';
+OutputData{end+1}.Name = 'SPACE1-1:Zone Air Temperature [C](TimeStep)';
 for idx = 1:NoOfOutput
     for idy = 1:size(RawData,2)
         if strcmp(RawData{1,idy}, OutputData{idx}.Name)
@@ -86,12 +90,12 @@ for idx = 1:NoOfFeatures
 end
 
 TrainingDays = 60;
-TrainingInput = Input(1:24*TrainingDays,:);
-TestingInput = Input(1+24*TrainingDays:end,:);
+TrainingInput = Input(1:(24*60/TimeStep)*TrainingDays,:);
+TestingInput = Input(1+(24*60/TimeStep)*TrainingDays:end,:);
 
 Output = cell2mat(OutputData{1}.Data);
-TrainingOutput = Output(1:24*TrainingDays,:);
-TestingOutput = Output(1+24*TrainingDays:end,:);
+TrainingOutput = Output(1:(24*60/TimeStep)*TrainingDays,:);
+TestingOutput = Output(1+(24*60/TimeStep)*TrainingDays:end,:);
 
 %% Optimal ARMAX model
 % Caution training data without autoregression features should be used
@@ -107,7 +111,7 @@ ARMAXTestingdata = iddata(TestingOutput, TestingInput, 3600);
 % nn = selstruc(V,0);
 
 % na = 0:5:25; nb = 1:5:26; nc = 0:5:25; nk = 0;
-na = 10; nb = 10; nc = 10; nk = 0;
+na = OrderOfAR; nb = OrderOfAR+1; nc = OrderOfAR; nk = 0;
 
 models = cell(length(na),length(nb),length(nc));
 MSE = zeros(length(na),length(nb),length(nc));
@@ -116,7 +120,7 @@ for ia = 1:length(na)
         for ic = 1:length(nc)
             models{ia,ib,ic} = armax(ARMAXTrainingdata, [na(ia), [nb(ib)*ones(1, NoOfInputs-2) 1 1], nc(ic), nk*ones(1, NoOfInputs)]);
             MSE(ia,ib,ic) = models{ia,ib,ic}.Report.Fit.MSE;
-            [ia,ib,ic]
+%             [ia,ib,ic]
         end
     end
 end
@@ -133,7 +137,7 @@ compare(ARMAXTestingdata, models{1,1,1});
 
 keyboard
 
-h = figure(6);
+h = figure(2);
 axesObjs = get(h, 'Children');  %axes handles
 dataObjs = get(axesObjs, 'Children'); %handles to low-level graphics objects in axes
 linedataObjs = get(dataObjs, 'Children');
@@ -143,7 +147,7 @@ ActualOutput = linedataObjs{2}.YData;
 ARMAXNRMSE = sqrt(mean((ARMAXOutput-ActualOutput).^2))/mean(ActualOutput);
 close(h);
 figure; hold on;
-title(['Order of AR: na = ' num2str(na)]);
+title(['Order of AR: na = ' num2str(na) ', nb = ' num2str(nb) ', nc = ' num2str(nc)]);
 h1 = plot(1:length(ActualOutput), ActualOutput, 'r');
 h2 = plot(1:length(ActualOutput), ARMAXOutput, 'b');
 legend([h1, h2], 'Actual', ['ARMAX ' num2str(ARMAXNRMSE,2)])
--- Code/AutoregressiveTree.m
+++ Code/AutoregressiveTree.m
@@ -5,11 +5,13 @@ clear
 filename = '5ZoneSteamBaseboard.csv';
 [~,~,RawData] = xlsread(filename);
 
-% First 48 numeric entries in the excel file correspond to design days.
+% First 48*60/TimeStep numeric entries in the excel file correspond to design days
 % These must be excluded. Higher start index can be chosen to account for
 % autoregrssive part
-OrderOfAR = 0; % less than 24 here
-StartIndex = 242;
+
+TimeStep = 15; % In mins
+OrderOfAR = 1*60/TimeStep; % less than 24 hrs here
+StartIndex = 1+2*24*60/TimeStep+24*60/TimeStep+1; % 1 for string names, 2 days for design days data, then autoregressive terms, then 1 for next index
 
 %% Organize the data
 
@@ -19,18 +21,18 @@ InputData = {};
 % Training Features without autoregressive contribution
 % Specify the features from EnergyPlus .idf output variables
 % TrainingData{end+1}.Name = '';
-InputData{end+1}.Name = 'Environment:Site Outdoor Air Drybulb Temperature [C](Hourly)';
-% InputData{end+1}.Name = 'SPACE1-1:Zone Air System Sensible Heating Rate [W](Hourly)';
-% InputData{end+1}.Name = 'SPACE1-1:Zone Air Heat Balance Air Energy Storage Rate [W](Hourly)';
-% InputData{end+1}.Name = 'SPACE1-1:Zone Total Internal Total Heating Rate [W](Hourly)';
-% InputData{end+1}.Name = 'Environment:Site Direct Solar Radiation Rate per Area [W/m2](Hourly)';
-InputData{end+1}.Name = 'FRONT-1:Surface Outside Face Incident Solar Radiation Rate per Area [W/m2](Hourly)';
-InputData{end+1}.Name = 'Environment:Site Outdoor Air Relative Humidity [%](Hourly)';
-InputData{end+1}.Name = 'Environment:Site Wind Speed [m/s](Hourly)';
-InputData{end+1}.Name = 'Environment:Site Wind Direction [deg](Hourly)';
-InputData{end+1}.Name = 'SPACE1-1:Zone People Occupant Count [](Hourly)';
-InputData{end+1}.Name = 'SPACE1-1:Zone Lights Total Heating Energy [J](Hourly)';
-InputData{end+1}.Name = 'SPACE1-1 BASEBOARD:Baseboard Total Heating Rate [W](Hourly)';
+InputData{end+1}.Name = 'Environment:Site Outdoor Air Drybulb Temperature [C](TimeStep)';
+% InputData{end+1}.Name = 'SPACE1-1:Zone Air System Sensible Heating Rate [W](TimeStep)';
+% InputData{end+1}.Name = 'SPACE1-1:Zone Air Heat Balance Air Energy Storage Rate [W](TimeStep)';
+% InputData{end+1}.Name = 'SPACE1-1:Zone Total Internal Total Heating Rate [W](TimeStep)';
+% InputData{end+1}.Name = 'Environment:Site Direct Solar Radiation Rate per Area [W/m2](TimeStep)';
+InputData{end+1}.Name = 'FRONT-1:Surface Outside Face Incident Solar Radiation Rate per Area [W/m2](TimeStep)';
+InputData{end+1}.Name = 'Environment:Site Outdoor Air Relative Humidity [%](TimeStep)';
+InputData{end+1}.Name = 'Environment:Site Wind Speed [m/s](TimeStep)';
+InputData{end+1}.Name = 'Environment:Site Wind Direction [deg](TimeStep)';
+InputData{end+1}.Name = 'SPACE1-1:Zone People Occupant Count [](TimeStep)';
+InputData{end+1}.Name = 'SPACE1-1:Zone Lights Total Heating Energy [J](TimeStep)';
+InputData{end+1}.Name = 'SPACE1-1 BASEBOARD:Baseboard Total Heating Rate [W](TimeStep)';
 
 
 for idx = 1:size(InputData,2)
@@ -55,9 +57,9 @@ for idx = 1:OrderOfAR
 end
 
 % Training Features for previous time stances of desired output
-% 'SPACE1-1:Zone Air Temperature [C](Hourly)';
+% 'SPACE1-1:Zone Air Temperature [C](TimeStep)';
 
-ARVariable.Name = 'SPACE1-1:Zone Air Temperature [C](Hourly)';
+ARVariable.Name = 'SPACE1-1:Zone Air Temperature [C](TimeStep)';
 
 for idx = 1:OrderOfAR
     for idy = 1:size(RawData,2)
@@ -71,12 +73,12 @@ end
 % Example: Start Day  = Monday
 % Mon = 0, Tue = 1, Wed = 2, Thur = 3, Fri = 4, Sat = 5, Sun = 6
 
-NoOfDays = NoOfDataPoints/24;
+NoOfDays = NoOfDataPoints/(24*60/TimeStep);
 InputData{end+1}.Name = 'Day';
 
 x1 = [0:NoOfDataPoints-1]';
-x2 = mod(x1,24);
-InputData{end}.Data = mod((x1-x2)/24,7);
+x2 = mod(x1,(24*60/TimeStep));
+InputData{end}.Data = mod((x1-x2)/(24*60/TimeStep),7);
 
 % Time of day
 InputData{end+1}.Name = 'Time';
@@ -84,14 +86,14 @@ for idy = 1:size(RawData,2)
     if strcmp(RawData{1,idy}, 'Date/Time')
         InputData{end}.Data = RawData(StartIndex:end,idy);
         chartime = char(InputData{end}.Data);
-        InputData{end}.Data = str2num(chartime(:,9:10)); %#ok<ST2NM>
+        InputData{end}.Data = str2num(chartime(:,9:10))*60+str2num(chartime(:,12:13)); %#ok<ST2NM>
     end
 end
 
 % Output Variables
 NoOfOutput = 1;
 OutputData = {};
-OutputData{end+1}.Name = 'SPACE1-1:Zone Air Temperature [C](Hourly)';
+OutputData{end+1}.Name = 'SPACE1-1:Zone Air Temperature [C](TimeStep)';
 for idx = 1:NoOfOutput
     for idy = 1:size(RawData,2)
         if strcmp(RawData{1,idy}, OutputData{idx}.Name)
@@ -113,15 +115,15 @@ for idx = 1:NoOfFeatures
 end
 
 TrainingDays = 60;
-TrainingInput = Input(1:24*TrainingDays,:);
-TestingInput = Input(1+24*TrainingDays:end,:);
+TrainingInput = Input(1:(24*60/TimeStep)*TrainingDays,:);
+TestingInput = Input(1+(24*60/TimeStep)*TrainingDays:end,:);
 
 Output = cell2mat(OutputData{1}.Data);
-TrainingOutput = Output(1:24*TrainingDays,:);
+TrainingOutput = Output(1:(24*60/TimeStep)*TrainingDays,:);
 
 %% Fit regression tree
 
-rtree = fitrtree(TrainingInput, TrainingOutput, 'MinLeafSize',20);
+rtree = fitrtree(TrainingInput, TrainingOutput, 'MinLeafSize',60);
 [~,~,~,bestLevel] = cvloss(rtree, 'SubTrees', 'all', 'KFold', 5);
 % view(rtree, 'Mode', 'graph');
 
@@ -135,7 +137,7 @@ brtree = fitensemble(TrainingInput, TrainingOutput, 'LSBoost', 500, 'Tree');
 
 %% Predict results
 
-ActualOutput = Output(1+24*TrainingDays:end,:);
+ActualOutput = Output(1+(24*60/TimeStep)*TrainingDays:end,:);
 
 rtreeOutput = predict(rtree, TestingInput);
 brtreeOutput = predict(brtree, TestingInput);
